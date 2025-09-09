4bc7a0309bcbd4ecb1eeb8660012500cf47a9d9c
alex <sasha.borchenko@gmail.com>
2025-09-09 15:45:48 +0300

    Add main basic handle logic

--- src/main.c
+++ src/main.c
@@ -1,7 +1,9 @@
 #include "sniffer.h"
+#include <stdio.h>
 
-int main()
+int main(int argc, char **argv)
 {
-	start_sniffer();
-	return 0;
+	const char *ifname = (argc > 1) ? argv[1] : "enp1s0"; // use your iface
+	printf("Listening on %s (promisc=1)...\n", ifname);
+	return start_sniffer(ifname, /*promisc=*/1, /*count=*/-1);
 }
