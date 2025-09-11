d1fb4ccc15a89d5b3a0a6ae83d6c5b98fcc9c14a
alex <sasha.borchenko@gmail.com>
2025-09-11 12:40:09 +0300

    Refactor: simplify start_sniffer and delegate ARP printing

--- src/sniffer.c
+++ src/sniffer.c
@@ -40,67 +40,31 @@ static int bind_to_iface(int fd, const char *ifname, int promisc) {
     return 0;
 }
 
-int start_sniffer(const char *ifname, int promiscuous, int count)
-{
+int start_sniffer(const char *ifname, int promiscuous, int count, const char *format) {
     int fd = socket(AF_PACKET, SOCK_RAW, htons(ETH_P_ALL));
-    if (fd < 0)
-    {
-        perror("socket");
-        return -1;
-    }
-
-    if (bind_to_iface(fd, ifname, promiscuous) < 0)
-    {
-        close(fd);
-        return -1;
-    }
+    if (fd < 0) { perror("socket"); return -1; }
+    if (bind_to_iface(fd, ifname, promiscuous) < 0) { close(fd); return -1; }
 
     uint8_t buf[2048];
-    while (count != 0)
-    {
-        struct sockaddr_ll from;
-        socklen_t fromlen = sizeof(from);
-        ssize_t n = recvfrom(fd, buf, sizeof(buf), 0, (struct sockaddr *)&from, &fromlen);
-
-        if (n < 0)
-        {
-            if (errno == EINTR)
-                continue;
+    while (count != 0) {
+        ssize_t n = recvfrom(fd, buf, sizeof(buf), 0, NULL, NULL);
+        if (n < 0) {
+            if (errno == EINTR) continue;
             perror("recvfrom");
             break;
         }
 
         struct arp_info info;
-        if (parse_arp(buf, (size_t)n, &info) == 0)
-        {
+        if (parse_arp(buf, (size_t)n, &info) == 0) {
             char smac[18], tmac[18], sip[INET_ADDRSTRLEN], tip[INET_ADDRSTRLEN];
             mac_to_str(info.sender_mac, smac);
             mac_to_str(info.target_mac, tmac);
-            ip_to_str(info.sender_ip, sip);
+            ip_to_str(info.sender_ip, sip);  // NBO -> string
             ip_to_str(info.target_ip, tip);
 
-            _Bool is_garp = (info.sender_ip == info.target_ip);
-
-
-            switch (info.operation)
-            {
-            case 1: // request
-                printf("%sARP request: Who has %s? Tell %s (src %s)\n",
-                       is_garp ? "[GARP] " : "", tip, sip, smac);
-                break;
-            case 2: // reply
-                printf("%sARP reply: %s is at %s\n",
-                       is_garp ? "[GARP] " : "", sip, smac);
-                break;
-            default:
-                printf("ARP op %u: %s -> %s\n",
-                       (unsigned)ntohs(info.operation), sip, tip);
-            }
-            fflush(stdout);
-            if (count > 0)
-                --count;
+            print_arp(&info, smac, tmac, sip, tip, format);
+            if (count > 0) --count;
         }
-        // TODO: handle non-ARP or malformed frames
     }
 
     close(fd);
