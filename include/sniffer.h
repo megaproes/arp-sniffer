e4bdfa1efda9c5962571a985af205116b0f61a86
alex <sasha.borchenko@gmail.com>
2025-09-11 12:19:42 +0300

    Add format option to start_sniffer func

--- include/sniffer.h
+++ include/sniffer.h
@@ -1,4 +1,4 @@
 #ifndef SNIFFER_H
 #define SNIFFER_H
-int start_sniffer(const char *ifname, int promiscuous, int count); // count<0 => infinite
+int start_sniffer(const char *ifname, int promiscuous, int count, const char *format); // count<0 => infinite
 #endif
